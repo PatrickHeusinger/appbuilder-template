25e3ecaa776f1855d3362c8d4cef0620bcddcc65
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2023-09-30 08:46:00 +0200

    migrate mantine 7

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import '@mantine/core/styles.css';
+import {Grid, Skeleton, Container} from '@mantine/core';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const child = <Skeleton height={140} radius="md" animate={false}/>;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Container my="md">
+            <Grid>
+                <Grid.Col span={{base: 12, xs: 4}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 8}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 8}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 4}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 3}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 3}}>{child}</Grid.Col>
+                <Grid.Col span={{base: 12, xs: 6}}>{child}</Grid.Col>
+            </Grid>
+        </Container>
+    )
 }
 
 export default App
--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <MantineProvider theme={theme}>
     <App />
+      </MantineProvider>
   </React.StrictMode>,
 )
